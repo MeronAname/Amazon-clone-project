21bd9184d1c4a33fd8ec418da09fecfb9a28878c
meron shola <meron@merons-MBP.lan>
2025-04-01 14:29:18 -0500

    first project

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,93 @@
+// const functions = require("firebase-functions");
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const { onRequest } = require("firebase-functions/v1/https");
+// const { log } = require("console");
+
+// dotenv.config();
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+// const app = express();
+
+// app.use(cors({ origin: true }));
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({
+//         message: "Success!"
+//     });
+// });
+
+// app.post("/payment/create", async (req, res) => {
+//     const total = parseInt(req.query.total);
+//     if (total > 0) {
+//         const paymentIntent = await stripe.paymentIntents.create({
+//             amount: total,
+//             currency: "usd",
+//         });
+
+//         console.log(paymentIntent);
+
+//         res.status(201).json({
+//             clientSecret: paymentIntent.client_secret
+//         });
+//     } else {
+//         res.status(403).json({
+//             message: "total must be greater than 0"
+//         });
+//     };
+// });
+
+// // Set global options for this function
+// exports.api = functions
+//     .runWith({
+//         maxInstances: 10,
+//     })
+//     .https.onRequest(app);
 const functions = require("firebase-functions");
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
-const { onRequest } = require("firebase-functions/v1/https");
-const { log } = require("console");
+
+// Load environment variables
 dotenv.config();
+
+// Initialize express app
+const app = express();
+
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-const app = express()
-app.use(cors({ origin: true }))
-app.use(express.json())
- 
+// Middleware
+app.use(cors({ origin: true }));
+app.use(express.json());
 
+// Routes
 app.get("/", (req, res) => {
     res.status(200).json({
-        message:"Success !"
+        message: "Success!"
     });
 });
+
 app.post("/payment/create", async (req, res) => {
-    const total = parseInt(req.query.total)
+    const total = parseInt(req.query.total);
     if (total > 0) {
         const paymentIntent = await stripe.paymentIntents.create({
             amount: total,
             currency: "usd",
-        })
+        });
 
         console.log(paymentIntent);
-        
+
         res.status(201).json({
-            clientSecret:paymentIntent.client_secret
-        })
+            clientSecret: paymentIntent.client_secret
+        });
     } else {
         res.status(403).json({
-            message: "total must be greater then 0"
-        })
-       
-    };
-})
+            message: "Total must be greater than 0"
+        });
+    }
+});
 
-    
-exports.api = onRequest(app);
-
+// Export the API to Firebase Functions
+exports.api = functions.https.onRequest(app);
--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,14 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-1eda2/us-central1/api"
+    // local instance of firebase functions
+    // baseURL: "http://127.0.0.1:5001/clone-1eda2/us-central1/api"
+    // deployed version of firebase function 
+    baseURL: "https://us-central1-clone-1eda2.cloudfunctions.net/api",
+
+    // deployed version of amazon server on render.com
+    // baseURL:"https://amazon-api-deploy-r3rk.onrender.com/",
+    
+
 });
 
 export {axiosInstance}
